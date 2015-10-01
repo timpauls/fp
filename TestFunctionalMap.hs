c471a0302cdcb5171a484e4da7407851d6028792
inf101368 <inf101368@fh-wedel.de>
2015-10-01 16:06:12 +0200

    2 tests for map

--- TestFunctionalMap.hs
+++ TestFunctionalMap.hs
@@ -6,12 +6,21 @@ import           Prelude            hiding (lookup)
 import qualified Prelude            as P
 import           Test.QuickCheck
 
+-- test insertion.
 prop_1 :: String -> Bool
-prop_1 xs = undefined
+prop_1 xs = 
+	case (lookup 5 (insert 5 xs empty)) of
+		Nothing -> False
+		Just ys -> ys == xs
 
+-- test deletion.
 prop_2 :: String -> Bool
-prop_2 xs = undefined
+prop_2 xs = 
+	case (lookup 5 (delete 5 (insert 5 xs empty))) of
+		Nothing -> True
+		Just _ -> False
 
+-- test union (??)
 prop_3 :: String -> Bool
 prop_3 xs = undefined
 
