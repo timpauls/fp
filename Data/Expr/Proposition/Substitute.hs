bb672ca88761927cbc8aed0b021ca964b1fd0aff
inf101368 <inf101368@fh-wedel.de>
2015-10-02 16:01:13 +0200

    free and subst variables

--- Data/Expr/Proposition/Substitute.hs
+++ Data/Expr/Proposition/Substitute.hs
@@ -10,9 +10,18 @@ import Data.Expr.Proposition.Types
 type VarEnv = [(Ident, Expr)]
 
 substVars :: VarEnv -> Expr -> Expr
-substVars env e = undefined
+substVars env (Lit a) = Lit a
+substVars env (Var a) = case lookup a env of
+							Nothing -> Var a
+							Just b -> b
+substVars env (Unary op e) = Unary op (substVars env e)
+substVars env (Binary op e1 e2) = Binary op (substVars env e1) (substVars env e2)
+
 
 freeVars :: Expr -> [Ident]
-freeVars  = undefined
+freeVars (Lit a) = []
+freeVars (Var a) = [a]
+freeVars (Unary _ e) = freeVars e
+freeVars (Binary _ e1 e2) = freeVars e1 `union` freeVars e2
 
 -- ----------------------------------------
